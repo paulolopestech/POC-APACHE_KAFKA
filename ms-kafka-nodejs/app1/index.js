e02ae8d172279de56a7bc0b7a2e690bfbcbf77a4
Paulo Lopes <paulo.junior@nouvenn.com>
2022-07-07 15:36:15 -0300

    tutorial 2

--- ms-kafka-nodejs/app1/index.js
+++ ms-kafka-nodejs/app1/index.js
@@ -5,26 +5,34 @@ const sequelize = require('sequelize');
 const app = express();
 app.use(express.json());
 
-const db = new sequelize(process.env.POSTGRES_URL);
-const User = db.define('user', {
-    name: sequelize.STRING,
-    email: sequelize.STRING,
-    password: sequelize.STRING
-});
+const dbsAreRunning = async () => {
+    const db = new sequelize(process.env.POSTGRES_URL);
 
-const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVERS});
-const producer = new kafka.Producer(client);
-producer.on('ready', () => {
-    console.log('producer ready');
-    app.post('/', (req, res) => {
-        producer.send([{
-            topic: process.env.KAFKA_TOPIC, 
-            messages: JSON.stringify(req.body)}], (err, data) => {
-            if(err) console.log(err);
-            else console.log(data);
-        })
+    const User = db.define('user', {
+        name: sequelize.STRING,
+        email: sequelize.STRING,
+        password: sequelize.STRING
     });
-})
 
+    db.sync({ force: true });
+
+    const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVERS});
+    const producer = new kafka.Producer(client);
+    producer.on('ready', async () => {
+        app.post('/', async (req, res) => {
+            producer.send([{
+                topic: process.env.KAFKA_TOPIC, 
+                messages: JSON.stringify(req.body)}], async (err, data) => {
+                if(err) console.log(err);
+                else {
+                    await User.create(req.body);
+                    res.send(req.body);
+                }
+            })
+        });
+    })
+}
+
+setTimeout(dbsAreRunning, 10000);
 
 app.listen(process.env.PORT);
